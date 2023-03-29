d347033d5616cead3ef7da29adedd96d7bbac7dd
Hasib Hossain <boraborhasib@gmail.com>
2023-03-29 21:45:20 +0600

    NavBar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import Card from './Components/Card/Card';
 import NavBar from './Components/NavBar/NavBar';
 
 function App() {
-
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish').then(res => res.json())
+      .then(data => setData(data.meals))
+  }, []);
+  // console.log(data);
   return (
     <div className="App">
       <NavBar></NavBar>
+      <div className="container">
+        <div className="">
+         <Card data={data}></Card>
+        </div>
+        <div className="">
+          <h1>Hello From Cart Section </h1>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+const Card = ({data}) => {
+    console.log('From Card', data)
+    return (
+        <div>
+           <div className="card">
+                
+           </div>
+        </div>
+    );
+};
+
+export default Card;
